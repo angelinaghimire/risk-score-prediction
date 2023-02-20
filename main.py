df1551dec1e164f66009aa2e89878f58e249f007
yugant.ghimire <yugant.ghimire@tai.com.np>
2023-02-20 19:59:56 +0545

    folder structuring

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.functions import basic_function
+# Implement streamlit part here
+# Save your model inside the model folder
+if __name__ == "__main__":
+    basic_function()
---
+++ src/functions.py
@@ -0,0 +1,3 @@
+def basic_function(name:str)->str:
+    """Returns the name"""
+    return name
